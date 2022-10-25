3279b2dcfc115e467f646d15ff1c81d832883cf7
Xiao Yu <me@xyu.io>
2022-10-25 23:09:20 +0000

    Add stub for instrumenting `WPDB`

---
+++ src/ElasticApm/Impl/AutoInstrument/WPDBAutoInstrumentation.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @noinspection PhpComposerExtensionStubsInspection */
+
+declare(strict_types=1);
+
+namespace Elastic\Apm\Impl\AutoInstrument;
+
+use Elastic\Apm\ElasticApm;
+use Elastic\Apm\Impl\Constants;
+use Elastic\Apm\Impl\Log\LogCategory;
+use Elastic\Apm\Impl\Log\Logger;
+use Elastic\Apm\Impl\Tracer;
+use Elastic\Apm\Impl\Util\TextUtil;
+use Elastic\Apm\SpanInterface;
+
+/**
+ * Code in this file is part of implementation internals and thus it is not covered by the backward compatibility.
+ *
+ * @internal
+ */
+final class WPDBAutoInstrumentation extends AutoInstrumentationBase
+{
+    use AutoInstrumentationTrait;
+}
